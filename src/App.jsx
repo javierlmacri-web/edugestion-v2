d77ef756427edfd778426a6d9f28b5ee4e47d647
javierlmacri-web <javier.l.macri@gmail.com>
2026-03-06 13:09:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react"; // update
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
